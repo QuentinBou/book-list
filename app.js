70074b42c4eb65d06754dbe452a699cb5bd50b8a
Quentin Bourgeois <quentintgc@live.fr>
2022-02-08 14:23:18 +0100

    fix empty form

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 import { Book, Stores, Ui } from "./utils.js";
 
-const store = new Stores()
+const store = new Stores();
 
-function reloadTrash(){
-    let ui = new Ui(
-        JSON.parse(localStorage.getItem("books")),
-        document.querySelector(".books-container-list")
-      );
-    setTimeout(() => {
-        let trashs = document.querySelectorAll('.fa-trash-alt')
-        trashs.forEach(el => {
-            el.addEventListener('click', e => {
-                ui.delete(document.getElementById(e.target.id))
-                store.deleteStore(e.target.id)
-            })
-        })
-    }, 500);
+function reloadTrash() {
+  let ui = new Ui(
+    JSON.parse(localStorage.getItem("books")),
+    document.querySelector(".books-container-list")
+  );
+  setTimeout(() => {
+    let trashs = document.querySelectorAll(".fa-trash-alt");
+    trashs.forEach((el) => {
+      el.addEventListener("click", (e) => {
+        ui.delete(document.getElementById(e.target.id));
+        store.deleteStore(e.target.id);
+      });
+    });
+  }, 500);
 }
 
 window.addEventListener("load", () => {
@@ -24,32 +24,64 @@ window.addEventListener("load", () => {
       JSON.parse(localStorage.getItem("books")),
       document.querySelector(".books-container-list")
     );
-    ui.display()
-    reloadTrash()
+    ui.display();
+    reloadTrash();
   }
 });
 
-let inputs = document.querySelectorAll('.form-group input')
-inputs.forEach(el => {
-    el.addEventListener('input', e => {
-        el.setAttribute('content', e.target.value)
-    })
-})
+let inputs = document.querySelectorAll(".form-group input");
+inputs.forEach((el) => {
+  el.addEventListener("input", (e) => {
+    el.setAttribute("content", e.target.value);
+  });
+});
 
-document.querySelector('form').addEventListener('submit', (e) => {
-    e.preventDefault()
-    let title = formTitle.getAttribute('content')
-    let author = formAuthor.getAttribute('content')
-    let isbn = formIsbn.getAttribute('content')
-    let book = new Book(title, author, isbn)
-    store.addStore(book)
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (
+    formTitle.getAttribute("content") != "" &&
+    formAuthor.getAttribute("content") != "" &&
+    formIsbn.getAttribute("content") != ""
+  ) {
+    let title = formTitle.getAttribute("content");
+    let author = formAuthor.getAttribute("content");
+    let isbn = formIsbn.getAttribute("content");
+    let book = new Book(title, author, isbn);
+    store.addStore(book);
     let ui = new Ui(
-        JSON.parse(localStorage.getItem("books")),
-        document.querySelector(".books-container-list")
+      JSON.parse(localStorage.getItem("books")),
+      document.querySelector(".books-container-list")
     );
-    ui.add(book)
-    inputs.forEach(el => {
-        el.value = ''
+    ui.add(book);
+    inputs.forEach((el) => {
+      el.value = "";
+      el.setAttribute("content", "");
+    });
+    reloadTrash();
+    Swal.fire({
+        title: 'Added !',
+        text: 'Book added to your List',
+        icon: 'success',
+        background: 'rgb(47, 47, 243)',
+        color: 'whitesmoke',
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
     })
-    reloadTrash()
-})
+  } else {
+    Swal.fire({
+        title: 'Error!',
+        text: 'Please complete the form',
+        icon: 'error',
+        background: 'rgb(47, 47, 243)',
+        color: 'whitesmoke',
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+    })
+  }
+});
